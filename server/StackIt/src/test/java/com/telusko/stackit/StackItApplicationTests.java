37c6438aa3ae198944cf582de6eab5c64c97b8fc
CodeWithAryansh07 <codewitharyansh07@gmail.com>
2025-07-12 08:57:13 +0530

    Spring Boot basic server added

---
+++ server/StackIt/src/test/java/com/telusko/stackit/StackItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.stackit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
